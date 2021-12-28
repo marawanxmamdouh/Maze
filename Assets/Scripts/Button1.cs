7ae9ebd39aadc40fface89f894cd4a8ef822bd02
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2021-12-28 08:57:38 +0200

    add rotation to Button1.cs

--- Assets/Scripts/Button1.cs
+++ Assets/Scripts/Button1.cs
@@ -3,13 +3,18 @@ public class Button1 : MonoBehaviour
 {
     public Transform btn;
     public Vector3 targetPosition;
-    public float smoothFactor = 50;
-
+    public float smoothFactor = 50f;
+    public float rotationsPerMinute = 10f;
+    void Update()
+    {
+        transform.Rotate(0.0f, (float)(6.0*rotationsPerMinute*Time.deltaTime),0f);
+    }
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name == "player")
         {
             btn.transform.position = Vector3.Lerp(btn.transform.position, targetPosition, Time.deltaTime * smoothFactor);
+            rotationsPerMinute = 0;
         }
     }
 }
