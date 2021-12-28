bca1f45a5c9164cc28c832783bebaad92bc848a8
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2021-12-28 08:10:58 +0200

    clean Button1.cs

--- Assets/Scripts/Button1.cs
+++ Assets/Scripts/Button1.cs
@@ -1,14 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Button_1 : MonoBehaviour
+public class Button1 : MonoBehaviour
 {
     public Transform btn;
-
     public Vector3 targetPosition;
     public float smoothFactor = 50;
-
 
     void OnCollisionEnter(Collision col)
     {
