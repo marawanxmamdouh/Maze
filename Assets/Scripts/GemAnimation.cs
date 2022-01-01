8a2dd6cb205e3ceea13b0946604f7e31cc936b2b
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-01-02 00:23:54 +0200

    add GemAnimation.cs

---
+++ Assets/Scripts/GemAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GemAnimation : MonoBehaviour
+{
+    // Before rendering each frame..
+    void Update () 
+    {
+        // Rotate the gem
+        transform.Rotate (new Vector3 (0, 0, 360) * Time.deltaTime/4);
+    }
+}
