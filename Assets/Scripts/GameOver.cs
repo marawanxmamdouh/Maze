4e3952625a7bc0d5007551b046a7160f9fde1427
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-01-01 15:06:10 +0200

    Add GameOver Script

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 playerPosition;
+    public int level = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = player.transform.position;
+        if (level == 0)
+        {
+            
+        }
+        if (playerPosition.y < 0)
+        {
+            //TODO Delay
+            player.transform.position = Vector3.Lerp(player.transform.position,new Vector3 (0.25f, 1f, 13f),1);
+            level--;
+            Debug.Log(level);
+        }
+    }
+}
