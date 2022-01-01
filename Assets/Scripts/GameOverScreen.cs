2657a81045d207513a5bdc65e49a6bc2075439c4
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-01-01 16:01:28 +0200

    Add GameOver Screen Script

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class GameOverScreen : MonoBehaviour
+    {
+        public Text scoreText;
+
+        public void Setup(int score)
+        {
+            gameObject.SetActive(true);
+            scoreText.text = score + " POINTS";
+        } 
+    }
+}
