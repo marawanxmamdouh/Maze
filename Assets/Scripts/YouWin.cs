595ffeeb5b4cf98eb8a0da5d846999afacb0221b
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-01-02 22:25:53 +0200

    ADD You Win Screen

---
+++ Assets/Scripts/YouWin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class YouWin : MonoBehaviour
+{
+    public YouWinScreen youWinScreen;
+    public GameObject player;
+
+    void Update()
+    {
+        if (PlayerController.score == 50)
+        {
+            youWinScreen.Setup();
+        }
+    }
+}
---
+++ Assets/Scripts/YouWinScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWinScreen : MonoBehaviour
+{
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void PlayAgainButton()
+    {
+        SceneManager.LoadScene("MinGame");
+        PlayerController.lives = 3;
+        PlayerController.score = 0;
+    }
+    
+    public void MainMenuButton()
+    {
+        //TODO add main menu
+        SceneManager.LoadScene("GameOver");
+    }
+}
