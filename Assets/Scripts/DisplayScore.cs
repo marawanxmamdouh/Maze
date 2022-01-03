40c8275a4edd360808a58767ea36381a8aea3283
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-01-03 03:08:37 +0200

    Display Lives (Hearts)

--- Assets/Scripts/DisplayScore.cs
+++ Assets/Scripts/DisplayScore.cs
@@ -5,9 +5,24 @@ using UnityEngine.UI;
 public class DisplayScore : MonoBehaviour
 {
     public Text scoreText;
-
+    public RawImage heart1;
+    public RawImage heart2;
+    public RawImage heart3;
+    
     void Update()
     {
         scoreText.text = PlayerController.score.ToString(); //+ " Points"
+        switch (PlayerController.lives)
+        {
+            case 2:
+                heart3.gameObject.SetActive(false);
+                break;
+            case 1:
+                heart2.gameObject.SetActive(false);
+                break;
+            case 0:
+                heart1.gameObject.SetActive(false);
+                break;
+        }
     }
 }
